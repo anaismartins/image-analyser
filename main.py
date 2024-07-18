a4061b494ba1423b24ecdead11fc65fd80b94913
Rui Alberto Martins <rui.alberto@solutio.pt>
2024-07-18 15:02:18 +0100

    now reading all files

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import cv2
+import os
 
-image = cv2.imread("data\\1.png")
+files = os.listdir("data")
+print(files)
 
-y = 45
-x = 130
-h = 295
-w = 1280
-crop_image = image[y:h, x:w]
+for file in files:
+    image = cv2.imread(f"data\\{file}")
 
-cv2.imwrite("output\\image.bmp", crop_image)
+    y = 45
+    x = 130
+    h = 295
+    w = 1280
+    crop_image = image[y:h, x:w]
+
+    cv2.imwrite(f"output\\{file[:-4]}.bmp", crop_image)
