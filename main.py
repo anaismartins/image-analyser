6e237733677c826f9bf713cc67dd3e4957ce2348
ana <anaisabelmartins@tecnico.ulisboa.pt>
2024-07-19 19:10:43 +0100

    added all to one image

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import cv2
 import os
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.pyplot import figure
+from PIL import Image
 
 files = os.listdir("data")
 
@@ -23,24 +25,49 @@ for file in files:
     else:
         gray = crop_image
 
+    gray = np.flip(gray, 0)
+
     hsumup = [0] * len(gray)
     vsumup = [0] * len(gray[0])
 
     #
-    #print (len(gray), " - ", len(gray[0]))
+    # print (len(gray), " - ", len(gray[0]))
 
     for column in range(len(gray)):
         for line in range(len(gray[column])):
             hsumup[column] += int(gray[column][line])
             vsumup[line] += int(gray[column][line])
 
-
+    figure(figsize=(2.95, 2.95), dpi=100)
     plt.plot(hsumup, np.arange(len(gray)), linewidth=1)
-    plt.savefig(f"output\\{file[:-4]}_hsumup.png")
+    plt.axis("off")
+    plt.tight_layout(pad=0)
+    plt.savefig(f"output\\{file[:-4]}_hsumup.png", bbox_inches="tight", pad_inches=0)
     plt.clf()
 
-    #print (vsumup)
+    # print (vsumup)
 
+    figure(figsize=(12.80, 2.95), dpi=100)
     plt.plot(vsumup, linewidth=1)
-    plt.savefig(f"output\\{file[:-4]}_vsumup.png")
+    plt.axis("off")
+    plt.tight_layout(pad=0)
+    plt.savefig(f"output\\{file[:-4]}_vsumup.png", bbox_inches="tight", pad_inches=0)
     plt.clf()
+
+    original_image = Image.open(f"output\\{file[:-4]}.bmp")
+    hsumup_image = Image.open(f"output\\{file[:-4]}_hsumup.png")
+    vsumup_image = Image.open(f"output\\{file[:-4]}_vsumup.png")
+
+    combined = Image.new(
+        "RGB",
+        (
+            original_image.width + hsumup_image.width,
+            original_image.height + vsumup_image.height,
+        ),
+        (255, 255, 255, 255),
+    )
+    combined.paste(original_image, (0, 0))
+    combined.paste(hsumup_image, (original_image.width, 0))
+    combined.paste(vsumup_image, (0, original_image.height))
+
+    combined.save(f"output\\{file[:-4]}_combined.png")
